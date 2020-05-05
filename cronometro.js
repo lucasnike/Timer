1414931ebaf7c3d0c27b7e088a978f2eb20e663d
Lucas Vinícius <50972547+lucasnike@users.noreply.github.com>
2020-05-05 10:56:46 -0300

    Removendo alguns bugs

--- cronometro.js
+++ cronometro.js
@@ -1,8 +1,8 @@
-let nodeList = document.querySelectorAll('span')
-let numeros = Array.from(nodeList)
+const nodeList = document.querySelectorAll('span')
+const numeros = Array.from(nodeList)
 
 let isStoped = false
-
+let contador = 0
 
 let s4 = 1
 let s3 = 0
@@ -13,41 +13,54 @@ function inverterIsStoped() {
     isStoped = !isStoped
 }
 
-function iniciar() {
-    isStoped = false
-    let interval = setInterval(() => {
-        if (isStoped === false) {
-    
-            if (s4 === 10 && s3 != 6) {
+function regularSetInterval() {
+    contador++
+    if (contador === 1) {
+        const iniciar = () => {
+            isStoped = false
+            let inter = setInterval(() => {
+                if (isStoped === false) {
             
-                s4 = 0
-                s3++
-            }
-            if (s3 === 6) {
-                s2++
-                s3 = 0
-            }
-            if (s2 === 10) {
-                s1++
-                s2 = 0
-            }
+                    if (s4 === 10 && s3 != 6) {
+                    
+                        s4 = 0
+                        s3++
+                    }
+                    if (s3 === 6) {
+                        s2++
+                        s3 = 0
+                    }
+                    if (s2 === 10) {
+                        s1++
+                        s2 = 0
+                    }
+                    
+                    numeros[3].innerHTML = s4++
+                    numeros[2].innerHTML = s3
+                    numeros[1].innerHTML = s2
+                    numeros[0].innerHTML = s1
+                }
+            }, 1000 ) 
             
-            numeros[3].innerHTML = s4++
-            numeros[2].innerHTML = s3
-            numeros[1].innerHTML = s2
-            numeros[0].innerHTML = s1
         }
-    }, 1000 ) 
+        return iniciar
+    } else {
+        return () => {}
+    }
 }
 
+
+
 function zerar(){
-    s4 = 0
+    s4 = 1
     s3 = 0
     s2 = 0
     s1 = 0
-    numeros[3].innerHTML = s4
-    numeros[2].innerHTML = s3
-    numeros[1].innerHTML = s2
-    numeros[0].innerHTML = s1
+    numeros[3].innerHTML = 0
+    numeros[2].innerHTML = 0
+    numeros[1].innerHTML = 0
+    numeros[0].innerHTML = 0
     isStoped = true
+    contador = 0
+    clearInterval()
 }
